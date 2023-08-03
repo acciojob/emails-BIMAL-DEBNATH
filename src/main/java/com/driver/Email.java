e20b7b2756cf56eadc7c1a9f08abff1709440cf5
BIMAL-DEBNATH <bimaldebnath8011@gmail.com>
2023-08-03 07:36:54 +0530

    FirstTry

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,33 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        int totalCharacter=0;
+        boolean upperLatter=false;
+        boolean lowerLatter=false;
+        boolean digit=false;
+        boolean specialChar=false;
+
+        for(char ch:newPassword.toCharArray()){
+            if(ch>='A' && ch<='Z'){
+                upperLatter=true;
+            }else if(ch>='a' && ch<='z'){
+                lowerLatter=true;
+            }else if (Character.isDigit(ch)){
+                digit=true;
+            }else{
+                specialChar=true;
+            }
+
+            totalCharacter++;
+        }
+
+        if((upperLatter && lowerLatter && digit && specialChar) && totalCharacter>=8){
+            if(this.password.equals(oldPassword)){
+                this.password=newPassword;
+            }
+        }
+
     }
+
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -5,6 +5,8 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+
 
 public class Workspace extends Gmail{
 
@@ -12,12 +14,15 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-
+        super(emailId, Integer.MAX_VALUE);
+        calendar=new ArrayList<>();
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
-
+       if(calendar.size()<Integer.MAX_VALUE){
+           calendar.add(meeting);
+       }
     }
 
     public int findMaxMeetings(){
@@ -26,5 +31,29 @@ public class Workspace extends Gmail{
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
 
+        if(calendar.size()==0)return 0;
+
+        ArrayList<Pair>maxMeeting=new ArrayList<>();
+
+        for(int i=0; i<calendar.size(); i++){
+           maxMeeting.add(Pair.of(calendar.get(i).getEndTime(),i));
+        }
+
+        Collections.sort(maxMeeting);
+
+
+        LocalTime time_limit= (LocalTime) maxMeeting.get(0).getLeft();
+
+        int ctn=1;
+
+        for(int i=1; i<maxMeeting.size(); i++){
+
+            if(calendar.get((Integer) maxMeeting.get(i).getRight()).getStartTime().compareTo(time_limit)>0){
+                ctn++;
+                time_limit= (LocalTime) maxMeeting.get(i).getLeft();
+            }
+        }
+
+          return ctn;
     }
 }
